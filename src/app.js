2cd4f4731a2db0e82c5cd528979832691810942c
Chris Doykov <swat1983@abv.bg>
2019-06-27 09:30:20 +0300

    Add about text

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About page',
+        title: 'About me',
         name: 'Chris Doykov'
     })
 });
